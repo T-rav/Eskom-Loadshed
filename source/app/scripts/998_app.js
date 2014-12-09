c6236363607c017fa833d41d73cf9a63796f8813
travis <tmfrisinger@gmail.com>
2014-12-09 21:35:54 +0200

    Added more stuff

--- source/app/scripts/998_app.js
+++ source/app/scripts/998_app.js
@@ -7,65 +7,30 @@
     var app = {
         init: function() {
 
-            // swap to false to show splash screen
-            var isDevEnv = true;
-            var loadScreenTTL = 4500;
-            var loadScreenInterval = 500;
-
             this.fixBottomMenuItemsForSmallerScreens();
-            this.slashScreen(isDevEnv,loadScreenTTL, loadScreenInterval);
-		    this.bootstrap();	
-            // register routing engine ;)
-			//var self = this;
-			//document.addEventListener('click', function(){self.routingEngine(event);}, false);
-            //self.handleBackButton();
-        },
-        bootstrap : function(){
-            
-            var message = new MessageModel();
-            var loadViewModel = new UpdateProgress(10, 10.5);
             var viewModel = new ViewModel();
-            
-            // -- load screen
-            this.bindLoader(message, loadViewModel);
-    
-            // bind the ui
+            this.fetchStatus(viewModel);
             this.bindApp(viewModel);
-
         },
-        bindLoader:function(message, loadViewModel){
-            setInterval(function(){loadViewModel.update(message);}, 400);
-            ko.applyBindings(message, document.getElementById("load"));
+        fetchStatus:function(viewModel){
+            $.ajax({
+                url : "http://stoneagetechnologies.com/eskomloadshed/status/?jsoncallback=?",
+                dataType : "jsonp",
+                crossDomain : true,
+                async: false,
+                success : function(data){
+                    viewModel.setMessageFromStatus(data);
+                },
+                error : function(){
+                    viewModel.setError();
+                }
+            }); 
         },
         bindApp:function(viewModel){
             
-            // settings
-            //ko.applyBindings(settingsViewModel, document.getElementById("defaults"));
-    
             // -- main
-           
             ko.applyBindings(viewModel, document.getElementById("main"));
             
-            // -- news
-            //ko.applyBindings(newsViewModel, document.getElementById("newsRegion"));
-            
-        },
-        slashScreen: function(isDevEnv, loadScreenTTL, loadScreenInterval) {
-            // define splash and content -id elements for this functionality
-            if(isDevEnv){
-                this.showMain();
-            }else{
-                this.showSplash();
-                setTimeout(function(){$('#load').collapse('hide'); 
-                    setTimeout(function(){$('#main').show();},loadScreenInterval);}
-                    ,loadScreenTTL);    
-            }
-        },
-        showSplash: function(){
-            $('#load').collapse('show');
-        },
-        showMain: function(){
-            $('#main').show();
         },
         fixBottomMenuItemsForSmallerScreens: function() {
             // if you have a ul.bottom, this helps to place it on smaller screens
@@ -79,28 +44,7 @@
             if (bottomListTop <= lastItemBottom) {
                 bottomList.css("position", "relative");
             }
-        },
-        handleBackButton : function(){
-            document.addEventListener("backbutton", function(e){
-               //if($.mobile.activePage.is('#market')){
-                   // exit if home page
-                   //e.preventDefault();
-                   //navigator.app.exitApp();
-               //}
-               //else {
-                   e.preventDefault();
-                   navigator.app.backHistory()
-               //}
-            }, false);
-        },
-		routingEngine:function(event){
-			// now we just need to handle routes and inject html fragments; )
-			var target = event.target;
-			//#/path/to/resource.html
-			if(!(target === undefined) && target.tagName.toLowerCase() === "a"){
-				console.log("TARGET->"+target.href);
-			}
-		}
+        }
     };
 
     document.addEventListener('deviceready', function() {
--- source/app/scripts/viewModel.js
+++ source/app/scripts/viewModel.js
@@ -1,5 +1,41 @@
 	function ViewModel(){
 		var self = this;
-		
+	
+		self.messageStatus = ko.observable(0);
+		self.message = ko.observable("Checking....");
+
+		self.isLvl0 =  ko.computed(function () {
+	        return self.messageStatus === 0;
+	    }, self);
+
+	    self.isLvl1 =  ko.computed(function () {
+	        return self.messageStatus === 1;
+	    }, self);
+
+		self.setMessageFromStatus = function(status){
+
+			if(status === 0){
+				self.message("No load shedding.");
+				self.messageStatus(0);
+			}else if(status === 1){
+				self.message("Stage 1 load shedding.");
+				self.messageStatus(1);
+			}else if(status === 2){
+				self.message("Stage 2 load shedding.");
+				self.messageStatus(2);
+			}else if(status === 3){
+				self.message("Stage 3 load shedding.");
+				self.messageStatus(4);
+			}else{
+				self.message("Unknown status?!");
+				self.messageStatus(5);
+			}
+		};
+
+		self.setError = function(){
+			self.message("Ensure your data is on.");
+			self.messageStatus(5);
+		};
+
 	}
 		
