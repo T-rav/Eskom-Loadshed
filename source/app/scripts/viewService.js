ee2eedf888b96bdd3233a57b4a0383081b0a9f8d
travis <tmfrisinger@gmail.com>
2015-01-12 21:55:08 +0200

    Updated the network status to working

--- source/app/scripts/viewService.js
+++ source/app/scripts/viewService.js
@@ -2,7 +2,9 @@ function ViewService(){
     var self = this;
 
     self.fetchData = function(viewModel, canDisplayMessage){
-		if(self.isNetworkAvailable()){
+		var networkStatus = self.isNetworkAvailable();
+		
+		if(networkStatus){
 			$.ajax({
 				url : "http://stoneagetechnologies.com/eskomloadshed/status2/?jsoncallback=?",
 				dataType : "jsonp",
@@ -26,6 +28,8 @@ function ViewService(){
 					viewModel.setError();
 				}
 			}); 
+		}else{
+			viewModel.setError();
 		}
     };
 	
@@ -41,18 +45,6 @@ function ViewService(){
 	
 	self.networkState = function(){
 		var networkState = navigator.connection.type;
-
-		/*
-		var states = {};
-		states[Connection.UNKNOWN]  = 'Unknown connection';
-		states[Connection.ETHERNET] = 'Ethernet connection';
-		states[Connection.WIFI]     = 'WiFi connection';
-		states[Connection.CELL_2G]  = 'Cell 2G connection';
-		states[Connection.CELL_3G]  = 'Cell 3G connection';
-		states[Connection.CELL_4G]  = 'Cell 4G connection';
-		states[Connection.CELL]     = 'Cell generic connection';
-		states[Connection.NONE]     = 'No network connection';
-		*/
 
 		return networkState;
 
