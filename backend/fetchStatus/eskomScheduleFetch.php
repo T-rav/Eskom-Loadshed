cfbeee0efbf3f7e6d43e2b08686b905fd8c3e3d7
travis <tmfrisinger@gmail.com>
2015-02-10 21:44:47 +0200

    Fixing up city data import

--- backend/fetchStatus/eskomScheduleFetch.php
+++ backend/fetchStatus/eskomScheduleFetch.php
@@ -1,25 +1,40 @@
 <?php
-
-	$letters = range('a','z');
-	// now insert data ;
+	
     $con = mysqli_connect("localhost", "loadshed", "FU#kESK0m\$zx!", "eskom");
+	
+	$query = "select * from municipality;";
+	$result = $con->query($query);
+	$rowCnt = $result->num_rows;
+	
+	for($i = 0; $i < $rowCnt; $i++){
+		$result->data_seek($i);
+		$data = $result->fetch_assoc();
 
-	foreach($letters as $letter){
-		echo "Processing [ $letter ] ";
-		$string = file_get_contents("data/news24/$letter.fetch");
-		$json=json_decode($string,true);
+		$id = $data["id"];
+		$prov = $data["province"];
+		$munic = $data["value"];
+		$json = fetchData($id);
 		foreach($json as $item){ 
-		    $placeName = strtoupper(trim($item['SubPlaceName'])); 
-		    $zoneId = $item['ZoneId'];
-		    $zoneName = strtoupper(trim($item['ZoneName']));
-		    $supplierName = strtoupper(trim($item['SupplierName']));
-		    $mainPlaceName = strtoupper(trim($item['MainPlaceName']));
-		    $province = strtoupper(trim($item['ProvinceName']));
-		    $lastUpdateTs = $item['ScheduleLastUpdated'];
-	  	    $sql = "insert into loadshed(subPlaceName, zoneId, zoneName,supplierName,  placeName, province, srcLastUpdateTs, srcName) values('$placeName',$zoneId, '$zoneName','$supplierName','$mainPlaceName','$province','$lastUpdateTs','NEWS24')";
-		    mysqli_query($con, $sql);	
+			
+			$placeName = strtoupper(trim($item['text'])); 
+			$id = $item['id'];
+			$sql = "insert into city(province, municipality, name, id) values('$prov','$munic','$placeName',$id)";
+			
+			mysqli_query($con, $sql);	
 		}
 	}
 
 	mysqli_close($con);
+	
+	
+	function fetchData($value){
+
+		$urlBase = "http://loadshedding.eskom.co.za/LoadShedding/GetSurburbData/?pageSize=90000&pageNum=1&searchTerm=&id=$value&_=1423590714343";
+
+		$contents = file_get_contents($urlBase);
+
+		$result = json_decode($contents, true);
+
+		return $result["Results"];
+	}
 ?>
