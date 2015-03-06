a3b86f5ea5d0bc996b91178ee9945f348657b836
Travis Frisinger <travis.frisinger@chillisoft.co.za>
2015-03-06 10:19:50 +0200

    More cleanup

--- source/dist/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Projects\Eskom - Load Shedding\source\dist\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Projects\Eskom - Load Shedding\source\dist\platforms\android\CordovaLib\AndroidManifest.xml \
